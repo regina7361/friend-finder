230d3d98729494191fc3b864e5dd760cd922cccb
regina7361 <regina7361@gmail.com>
2019-04-27 10:09:29 -0700

    added code

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+module.exports = {
+	array: [
+		{
+			name: 'Antman',
+			photo: 'public/images/antman.jpg',
+			scores: [
+			"3",
+            "5",
+            "5",
+            "1",
+            "5",
+            "2", 
+            "5",
+            "5",
+			"3",
+            "5", 
+        ]
+		},
+		{
+			name: 'Black Panther',
+			photo: 'public/images/blackpanther.jpg',
+			scores: [
+			"4",
+			"5",
+			"3",
+			"1",				
+			"4",
+			"3",
+			"4",
+			"5",
+			"4",
+			"5",
+			]
+		},
+		{
+			name: 'Black Widow',
+			photo: 'public/images/blackwidow.jpg',
+			scores: [
+	    	"5",
+			"5",
+			"5",
+			"1",
+			"5",
+			"1",
+			"5",
+			"5",
+			"5",
+	        "5",
+			]
+		},
+		{
+			name: 'Captain America',
+			photo: 'public/images/captainamerica.jpg',
+			scores: [
+			"4",
+            "4",
+            "4",
+            "2",
+            "3",
+            "3",
+            "1",
+            "5",
+			"3",
+			"5",
+		]
+		},
+		{
+			name: 'Captain Marvel',
+			photo: 'public/images/captainmarvel.jpg',
+			scores: [
+			"5",
+            "1",
+            "4",
+            "2",
+            "2",
+            "2",
+            "3",
+            "5",
+			"1",
+            "5",
+        ]
+        },
+        {
+			name: 'Hawkeye',
+			photo: 'public/images/hawkeye.jpg',
+			scores: [
+			"5",
+            "1",
+            "4",
+            "2",
+            "2",
+            "2",
+            "5",
+            "5",
+			"1",
+            "5",
+        ]
+        },
+        {
+			name: 'Hulk',
+			photo: 'public/images/hulk.jpg',
+			scores: [
+			"5",
+            "1",
+            "5",
+            "2",
+            "2",
+            "2",
+            "3",
+            "5",
+			"1",
+            "5",
+        ]
+        },
+        {
+			name: 'Spiderman',
+			photo: 'public/images/spiderman.jpg',
+			scores: [
+			"5",
+            "1",
+            "4",
+            "2",
+            "2",
+            "2",
+            "3",
+            "5",
+			"1",
+            "5",
+        ]
+        },
+        {
+			name: 'Thor',
+			photo: 'public/images/thor.jpg',
+			scores: [
+			"1",
+            "5",
+            "3",
+            "2",
+            "2",
+            "2",
+            "3",
+            "5",
+			"1",
+            "5",
+        ]
+        },
+        {
+			name: 'Ironman',
+			photo: 'public/images/tonystark.jpg',
+			scores: [
+			"3",
+            "5",
+            "5",
+            "5",
+            "2",
+            "2",
+            "3",
+            "5",
+			"1",
+            "5",
+        ]
+		},
+	]
+}
